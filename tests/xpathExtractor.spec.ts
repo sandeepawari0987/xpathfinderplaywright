ed80a4ed24d7416a14aeeaf7699f0ee994e93c55
Sandeep Awari <sandeep.awari87@gmail.com>
2026-06-21 14:09:09 +0530

    Tesxt cheking for commit message

--- tests/xpathExtractor.spec.ts
+++ tests/xpathExtractor.spec.ts
@@ -4,4 +4,5 @@ import { getAllXPaths } from '../utils/xpathExtractor';
 test('Extract all XPaths from Example.com', async ({ browser }) => {
   const url = process.env.TEST_URL || 'https://selectorshub.com/xpath-practice-page/';
   await getAllXPaths(browser, url);
+  console.log('XPaths extracted successfully');
 });
