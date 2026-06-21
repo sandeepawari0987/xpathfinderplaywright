1fe2903692f48832e501c09c1a4ad43ab3534d7a
Sandeep Awari <sandeep.awari87@gmail.com>
2026-06-21 14:42:35 +0530

    demoqa locators

--- tests/xpathExtractor.spec.ts
+++ tests/xpathExtractor.spec.ts
@@ -2,7 +2,7 @@ import { test } from '@playwright/test';
 import { getAllXPaths } from '../utils/xpathExtractor';
 
 test('Extract all XPaths from Example.com', async ({ browser }) => {
-  const url = process.env.TEST_URL || 'https://www.amazon.in/';
+  const url = process.env.TEST_URL || 'https://demoqa.com/';
   await getAllXPaths(browser, url);
   console.log('XPaths extracted successfully');
 });
