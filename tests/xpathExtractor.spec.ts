f4cc2b33d146b9d040e0954e438dfcf7e44a9d83
Sandeep Awari <sandeep.awari87@gmail.com>
2026-06-21 14:30:58 +0530

    Amazon locators

--- tests/xpathExtractor.spec.ts
+++ tests/xpathExtractor.spec.ts
@@ -2,7 +2,7 @@ import { test } from '@playwright/test';
 import { getAllXPaths } from '../utils/xpathExtractor';
 
 test('Extract all XPaths from Example.com', async ({ browser }) => {
-  const url = process.env.TEST_URL || 'https://selectorshub.com/xpath-practice-page/';
+  const url = process.env.TEST_URL || 'https://www.amazon.in/';
   await getAllXPaths(browser, url);
   console.log('XPaths extracted successfully');
 });
